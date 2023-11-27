e5f26f2a146485dc36450f2b4ac09967b3930776
virgarto <iamvirginiag@gmail.com>
2023-11-27 15:15:35 +0100

    restart login bbdd

--- conexion_bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    // Creación de variables
-    $conexion=new mysqli("localhost", "root", "", "3306");
-    $conexion->set_charset("utf8");
-
-    
-?>
--- controlador.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(!empty($_POST("loginBtn"))){
-        //Verificamos si existe la información introducida en el login
-        if(empty($_POST("username") and empty($_POST("password")))){
-            echo "Los campos están vacíos!"
-        }
-    }
-?>
