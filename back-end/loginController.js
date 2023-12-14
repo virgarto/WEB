821fc272a84f5475d8f7044623686de60fdf5675
virgarto <iamvirginiag@gmail.com>
2023-12-14 20:07:06 +0100

    otra respuesta login

--- back-end/loginController.js
+++ back-end/loginController.js
@@ -19,3 +19,53 @@ function loginUsuario(user, password) {
 }
 
 module.exports = loginUsuario;
+
+/*
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Login</title>
+</head>
+<body>
+ <h1>Login</h1>
+ <form id="loginForm">
+ <label for="username">Usuario:</label>
+ <input type="text" id="username" name="username" required><br>
+ <label for="password">Contraseña:</label>
+ <input type="password" id="password" name="password" required><br>
+ <button type="submit">Iniciar sesión</button>
+ </form>
+ <p id="message"></p>
+
+ <script>
+ document.getElementById('loginForm').addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   var xhr = new XMLHttpRequest();
+   xhr.open('POST', '/login', true);
+   xhr.setRequestHeader('Content-Type', 'application/json');
+
+   xhr.onload = function() {
+    if (xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.success) {
+        document.getElementById('message').textContent = 'Login exitoso';
+      } else {
+        document.getElementById('message').textContent = 'Usuario o contraseña incorrectos';
+      }
+    } else {
+      document.getElementById('message').textContent = 'Error: ' + xhr.status;
+    }
+   };
+
+   var data = {
+     username: document.getElementById('username').value,
+     password: document.getElementById('password').value
+   };
+
+   xhr.send(JSON.stringify(data));
+ });
+ </script>
+</body>
+</html>
+*/
