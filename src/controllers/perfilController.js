40f5c5271e22b178105aa74b703f497d44c06988
virgarto <iamvirginiag@gmail.com>
2024-01-29 18:41:17 +0100

    muestra de mensaje de error en el form

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -8,19 +8,15 @@ function editUser (req, res){
 }
 
 function editPatinador (req, res){
-    console.log('hola :/');
     const data = req.body;
 
     req.getConnection((err, conn) =>{
         conn.query('SELECT * FROM users WHERE email = ?', [req.session.email] , (err, userData) => {
-            console.log('primera consulta:/');
             if(userData.length > 0){
-                console.log('pilla que ha metido info :/');
                 if(req.body.password){
                     bcrypt.hash(data.password, 12).then(hash => {
                         data.password = hash; 
 
-                        console.log(data);
                         conn.query('UPDATE users SET username = ?, password = ?, fecha_nacimiento = ?, estado = ? WHERE email = ?', [data.username, data.password, data.fecha_nacimiento, data.estado, req.session.email]);
                         req.session.name = data.username;
                         res.redirect('/');
@@ -28,7 +24,7 @@ function editPatinador (req, res){
                 }
             }
             else{
-                console.log('casi colega :/');
+                console.log('estoy aqui');
                 res.render('editUserForm', {error: 'Error: No has hecho ningún cambio.'});
             }
             
