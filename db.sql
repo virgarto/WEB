f6242016232241489febbfda051cf5b134814218
virgarto <iamvirginiag@gmail.com>
2024-02-07 18:06:37 +0100

    categoria tabla

---
+++ db.sql
@@ -0,0 +1,14 @@
+/* INSERTAMOS VALUES EN LA TABLA DE CATEGORIA */
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2004, 'Senior', 'Senior', 'Senior');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2005, 'Junior', 'Senior', 'Senior');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2006, 'Junior', 'Junior', 'Senior');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2007, 'Juvenil', 'Junior', 'Junior');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2008, 'Cadete', 'Juvenil', 'Junior');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2009, 'Cadete', 'Cadete', 'Juvenil');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2010, 'Infantil', 'Cadete', 'Cadete');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2011, 'Infantil', 'Infantil', 'Cadete');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2012, 'Alevín', 'Infantil', 'Infantil');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2013, 'Alevín', 'Alevín', 'Infantil');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2014, 'Benjamin', 'Alevín', 'Alevín');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2015, 'Benjamin', 'Benjamin', 'Alevín');
+INSERT INTO categoria (anyo, cat_2023, cat_2024, cat_2025) VALUES (2016, '', 'Benjamin', 'Benjamin');
