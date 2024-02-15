60ef9e6430dec590b79bfd390dfabf2372f54c5d
virgarto <iamvirginiag@gmail.com>
2024-02-15 18:04:23 +0100

    cambio form edit

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcrypt');
 
-function editUser (req, res, userData){
+function editUser (req, res){
     if(req.session.rol != 'Entrenador'){
         res.render('editUserForm', {});
-        console.log('Carga: ' + userData);
     }
         
     else    
@@ -12,7 +11,6 @@ function editUser (req, res, userData){
 
 function editPatinador (req, res){
     const data = req.body;
-    console.log(userData);
 
     req.getConnection((err, conn) =>{
         conn.query('SELECT * FROM users WHERE email = ?', [req.session.email] , (err, userData) => {
@@ -22,8 +20,17 @@ function editPatinador (req, res){
                     bcrypt.hash(data.password, 12).then(hash => {
                         data.password = hash; 
 
-
-                        conn.query('UPDATE users SET username = ?, password = ?, estado = ? WHERE email = ?', [data.username, data.password, data.estado, req.session.email]);
+                        if(data.username != req.session.username){
+                            conn.query('UPDATE users SET username = ? WHERE email = ?', [data.username, req.session.email]);
+                            console.log(data.username);
+                        }
+                        if(data.password.length > 0){
+                            conn.query('UPDATE users SET password = ? WHERE email = ?', [data.password, req.session.email]);
+                        }
+                        if(data.estado != req.session.estado){
+                            conn.query('UPDATE users SET estado = ? WHERE email = ?', [data.estado, req.session.email]);
+                        }
+                        //conn.query('UPDATE users SET username = ?, password = ?, estado = ? WHERE email = ?', [data.username, data.password, data.estado, req.session.email]);
                         req.session.name = data.username;
                         req.session.estado = data.estado;
                         res.redirect('/');
