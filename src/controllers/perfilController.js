c88ba59d5e290a398fc525d18813cf94943f59af
virgarto <iamvirginiag@gmail.com>
2024-02-28 16:56:20 +0100

    actualizar usuario arreglado

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const e = require('express');
 
 function editUser (req, res){
     res.render('editUserForm', { name: req.session.name,
@@ -14,33 +15,31 @@ function editPatinador (req, res){
         conn.query('SELECT * FROM users WHERE email = ?', [req.session.email] , (err, userData) => {
             
             if(userData.length > 0){
-                if(req.body.password){
+                if(data.password && data.password.length > 0){
                     bcrypt.hash(data.password, 12).then(hash => {
                         data.password = hash; 
 
-                        if(data.username.length > 0){
-                            conn.query('UPDATE users SET username = ? WHERE email = ?', [data.username, req.session.email]);
+                        if(data.username && data.username.length > 0){
+                            conn.query('UPDATE users SET username = ?, password = ? WHERE email = ?', [data.username, data.password,req.session.email]);
                             console.log(data.username);
                         }
-                        if(data.password.length > 0){
+                        else{
                             conn.query('UPDATE users SET password = ? WHERE email = ?', [data.password, req.session.email]);
                         }
-                        if(data.estado != req.session.estado){
-                            console.log("data " + data.estado);
-                            conn.query('UPDATE users SET estado = ? WHERE email = ?', [data.estado, req.session.email]);
-                        }
                         //conn.query('UPDATE users SET username = ?, password = ?, estado = ? WHERE email = ?', [data.username, data.password, data.estado, req.session.email]);
-                        req.session.name = data.username;
-                        req.session.estado = data.estado;
-                        req.session.password = data.password;
-                        res.redirect('/');
                     });
+                }else if(data.username && data.username.length > 0){
+                        conn.query('UPDATE users SET username = ? WHERE email = ?', [data.username, req.session.email]);
                 }
+
+                req.session.name = data.username;
+                req.session.password = data.password;
+                res.redirect('/');
+
             }
             else{
                 res.render('editUserForm', {error: 'Error: No has hecho ningún cambio.'});
             }
-            
         })
     });
 }
