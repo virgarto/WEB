c149af8536b0f443c3e035fe132aacb22dd231de
virgarto <iamvirginiag@gmail.com>
2024-03-04 19:44:28 +0100

    backend form

--- src/controllers/EntreneController.js
+++ src/controllers/EntreneController.js
@@ -2,6 +2,13 @@ function goToEntreneForm (req, res){
     res.render('newEntreneDanza');
 }
 
+function createEntreneDanza(req, res){
+    const data = req.body;
+    
+    console.log(data);
+}
+
 module.exports = {
     goToEntreneForm,
+    createEntreneDanza,
 }
--- src/routes/entrene.js
+++ src/routes/entrene.js
@@ -3,6 +3,7 @@ const express = require('express');
 const EntreneController = require('../controllers/EntreneController');
 const router = express.Router();
 
-router.get('/newEntrene', EntreneController.goToEntreneForm);
+router.get('/newEntreneDanza', EntreneController.goToEntreneForm);
+router.post('/newEntreneDanza', EntreneController.createEntreneDanza);
 
 module.exports = router;
