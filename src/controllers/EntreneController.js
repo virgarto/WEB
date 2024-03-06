1775133ed2009fe04beedc2a85086cd3c39baa64
virgarto <iamvirginiag@gmail.com>
2024-03-06 18:44:00 +0100

    FORM ENTRENE

--- src/controllers/EntreneController.js
+++ src/controllers/EntreneController.js
@@ -4,6 +4,20 @@ function goToEntreneForm (req, res){
 
 function createEntreneDanza(req, res){
     const {travellingB, travelling1, travelling2, travelling3, travelling4} = req.body;
+    const {clusterB, cluster1, cluster2, cluster3, cluster4} = req.body;
+    const {key_point1, key_point2, key_point3, key_point4} = req.body;
+    const {ASqB, ASq1, ASq2, ASq3, ASq4} = req.body;
+    const {DStepSqB, DStepSq1, DStepSq2, DStepSq3, DStepSq4} = req.body;
+    const {FoSqB, FoSq1, FoSq2, FoSq3, FoSq4} = req.body;
+    const {ChStSq} = req.body;
+    const {BrDExtDetras, BrDExtDelante, BrDIntDetras, BrDIntDelante} = req.body;
+    const {BrIExtDetras, BrIExtDelante, BrIIntDetras, BrIIntDelante} = req.body;
+    const {CoDExtDetras, CoDExtDelante, CoDIntDetras, CoDIntDelante} = req.body;
+    const {CoIExtDetras, CoIExtDelante, CoIIntDetras, CoIIntDelante} = req.body;
+    const {RoDExtDetras, RoDExtDelante, RoDIntDetras, RoDIntDelante} = req.body;
+    const {RoIExtDetras, RoIExtDelante, RoIIntDetras, RoIIntDelante} = req.body;
+    const {LoDExtDetras, LoDExtDelante, LoDIntDetras, LoDIntDelante} = req.body;
+    const {LoIExtDetras, LoIExtDelante, LoIIntDetras, LoIIntDelante} = req.body;
     
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO travelling (travellingB, travelling1, travelling2, travelling3, travelling4) VALUES (?, ?, ?, ?, ?)', [travellingB, travelling1, travelling2, travelling3, travelling4]);
