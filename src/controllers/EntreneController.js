2f05a6ce10bfcf59cdb9d3959da7e1ff99394996
virgarto <iamvirginiag@gmail.com>
2024-03-26 13:48:25 +0100

    genera informe danza correcto

--- src/controllers/EntreneController.js
+++ src/controllers/EntreneController.js
@@ -13,8 +13,22 @@ function getInforme(req, res){
 
     const placeholders = [];
     const values = [];
+    const tablasDanza = {
+        'art_foot_sequence': ['ASqB', 'ASq1', 'ASq2', 'ASq3', 'ASq4'],
+        'bracket_derecho': ['BrDExtDetras', 'BrDExtDelante', 'BrDIntDetras', 'BrDIntDelante'], 
+        'bracket_izquierdo': ['BrIExtDetras', 'BrIExtDelante', 'BrIIntDetras', 'BrIIntDelante'], 
+        'choreo_step_sequence': ['ChStSq'], 
+        'cluster': ['clusterB', 'cluster1', 'cluster2', 'cluster3', 'cluster4'], 
+        'counter_derecho': ['CoDExtDetras', 'CoDExtDelante', 'CoDIntDetras', 'CoDIntDelante'], 
+        'counter_izquierdo': ['CoIExtDetras', 'CoIExtDelante', 'CoIIntDetras', 'CoIIntDelante'], 
+        'dance_step_sequence': ['DStepSqB', 'DStepSq1', 'DStepSq2', 'DStepSq3', 'DStepSq4'], 
+        'footwork_sequence': ['FoSqB', 'FoSq1', 'FoSq2', 'FoSq3', 'FoSq4'], 
+        'loop_derecho': ['LoDExtDetras', 'LoDExtDelante', 'LoDIntDetras', 'LoDIntDelante'], 
+        'loop_izquierdo': ['LoIExtDetras', 'LoIExtDelante', 'LoIIntDetras', 'LoIIntDelante'], 
+        'pattern_sequence': ['key_point1', 'key_point2', 'key_point3', 'key_point4'], 
+        'rocker_derecho': ['RoDExtDetras', 'RoDExtDelante', 'RoDIntDetras', 'RoDIntDelante'], 
+        'rocker_izq': ['RoIExtDetras', 'RoIExtDelante', 'RoIIntDetras', 'RoIIntDelante']};
 
-    let BrDExtDetras_avg, BrDExtDelante_avg, BrDIntDetras_avg, BrDIntDelante_avg;
     req.getConnection((err,conn)=> {
         if(modalidad ==  'danza'){
             conn.query('SELECT * FROM entrenamiento_danza WHERE fecha > ? AND fecha < ?', [fecha_ini, fecha_fin], (err, entrenes_danza) => {
@@ -22,26 +36,25 @@ function getInforme(req, res){
                     console.log('Error al obtener listado de entrenamientos Danza: ' + err);
                 }
                 else{
+                    console.log(entrenes_danza);
                     for (let i = 0; i < entrenes_danza.length; i++) {
                         placeholders.push('?');
                         values.push(entrenes_danza[i].id_bracket_der);
                     }
-                    const sql = 'SELECT AVG(BrDExtDetras) AS BrDExtDetras_avg, AVG(BrDExtDelante) AS BrDExtDelante_avg, AVG(BrDIntDetras) AS BrDIntDetras_avg, AVG(BrDIntDelante) AS BrDIntDelante_avg FROM (SELECT BrDExtDetras, BrDExtDelante, BrDIntDetras, BrDIntDelante FROM bracket_derecho WHERE id IN ('+ placeholders.join(',')+ ') ) AS subquery'
-                    conn.query(sql, values, (err, avg_bracket_der) => {
-                        if (err) {
-                          console.error(err);
-                          return;
-                        }
-                      
-                        console.log(avg_bracket_der);
-                    });
-                    /*conn.query('SELECT AVG(BrDExtDetras) AS BrDExtDetras_avg, AVG(BrDExtDelante) AS BrDExtDelante_avg, AVG(BrDIntDetras) AS BrDIntDetras_avg, AVG(BrDIntDelante) AS BrDIntDelante_avg FROM (SELECT BrDExtDetras, BrDExtDelante, BrDIntDetras, BrDIntDelante FROM bracket_derecho WHERE id= ? union SELECT BrDExtDetras, BrDExtDelante, BrDIntDetras, BrDIntDelante FROM bracket_derecho WHERE id= ?) AS subquery;', [entrenes_danza[0].id_bracket_der, entrenes_danza[1].id_bracket_der], (err, avg_bracket_der) => {
-                        console.log(avg_bracket_der);
-                        BrDExtDetras_avg = avg_bracket_der[0].BrDExtDetras_avg;
-                        BrDExtDelante_avg = avg_bracket_der[0].BrDExtDelante_avg;
-                        BrDIntDetras_avg = avg_bracket_der[0].BrDIntDetras_avg;
-                        BrDIntDelante_avg = avg_bracket_der[0].BrDIntDelante_avg;
-                    })*/
+                    for(let tablasName in  tablasDanza) {
+                        const columnNames = tablasDanza[tablasName];
+                        const sql = 'SELECT ' + columnNames.map(c => 'AVG('+ c +') AS '+ c + '_avg ').join(',') + ' FROM ( SELECT ' + columnNames.join(',') + ' FROM ' + tablasName + ' WHERE id IN ('+ placeholders.join(',')+ ') ) AS subquery'; 
+                    
+                    //const  = 'SELECT AVG(BrDExtDetras) AS BrDExtDetras_avg, AVG(BrDExtDelante) AS BrDExtDelante_avg, AVG(BrDIntDetras) AS BrDIntDetras_avg, AVG(BrDIntDelante) AS BrDIntDelante_avg FROM (SELECT BrDExtDetras, BrDExtDelante, BrDIntDetras, BrDIntDelante FROM bracket_derecho WHERE id IN ('+ (',')+ ') ) AS subquery'
+                        conn.query(sql, values, (err, avg_data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(tablasName, avg_data);
+                        });
+                    }
                 }
             });
         }else{
