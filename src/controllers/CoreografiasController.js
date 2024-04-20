3a8e7a870493d0040478fd342bc9589d91b7ee64
virgarto <iamvirginiag@gmail.com>
2024-04-20 13:44:36 +0200

    pasar el code a boton intento (no funciona)

--- src/controllers/CoreografiasController.js
+++ src/controllers/CoreografiasController.js
@@ -2,6 +2,11 @@ function goToDiscoCortoForm (req, res){
     res.render('discoCortoForm', {rol: req.session.rol, categoria: req.session.categoria, name:  req.session.name}); 
 }
 
+function addElementInForm (req, res){
+    res.render('addElements', {rol: req.session.rol});
+}
+
 module.exports ={
     goToDiscoCortoForm,
+    addElementInForm,
 }
--- src/routes/coreos.js
+++ src/routes/coreos.js
@@ -4,5 +4,6 @@ const CoreografiasController = require('../controllers/CoreografiasController');
 const router = express.Router();
 
 router.get('/discoCortoForm', CoreografiasController.goToDiscoCortoForm);
+router.get('/addElements', CoreografiasController.addElementInForm);
 
 module.exports = router;
