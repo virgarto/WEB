4fc2b7aea09da78ef5c909ac1d8a5fee20028aa0
virgarto <iamvirginiag@gmail.com>
2024-04-23 19:41:42 +0200

    tratando de diferenciar el codigo en el controller

--- src/controllers/CoreografiasController.js
+++ src/controllers/CoreografiasController.js
@@ -11,8 +11,19 @@ function goToaddElementInForm (req, res){
 function addElement(req, res){
     // Formulario para el SJu
     const selectedSalto = req.body.salto;
-
+    const code = req.body.code;
     console.log("Salto: " + selectedSalto);
+    console.log(code);
+/*
+    req.getConnection((error, conn) =>{
+        conn.query('SELECT rating_base AS value FROM saltos_base WHERE salto_nombre = ?', [selectedSalto], (error, base) =>{
+            console.log("Valoración: " + base[0].value);
+            let base_salto = base[0].value;
+
+            // Cargamos el formulario base y pasamos los valores 
+            res.render('discoCortoForm', {salto: selectedSalto, base_salto});
+        })
+    })*/
     
 }
 
