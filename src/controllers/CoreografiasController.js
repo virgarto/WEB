2f194f0e756963408dd62d5b3c76952047a29ff1
virgarto <iamvirginiag@gmail.com>
2024-05-07 17:08:17 +0200

    creacion de programa largo

--- src/controllers/CoreografiasController.js
+++ src/controllers/CoreografiasController.js
@@ -1,4 +1,8 @@
 function goToDiscoCortoForm (req, res){
+    const categoria = req.session.categoria
+    if(categoria == 'Alevín' || categoria == 'Benjamin'){
+        res.render('coreografías', {rol: req.session.rol, msg: 'Programa no disponible para la categoría Alevín y Benjamin'});
+    }
     res.render('discoCortoForm', {rol: req.session.rol, categoria: req.session.categoria, name:  req.session.name}); 
 }
 
@@ -15,10 +19,11 @@ function goToaddElementInForm (req, res){
     res.render('addElements', {rol: req.session.rol, fila, codigo, name, categoria});
 }
 
-let rowsCortoLibre = [
-    
-];
+let rowsCortoLibre = [];
+let rowsLargoLibre = [];
 let sumaBASE = 0;
+
+
 
 function addElement(req, res){
     const fila = req.body.fila;
@@ -39,6 +44,7 @@ function addElement(req, res){
             // Obtenemos BASE del salto seleccionado
             conn.query('SELECT rating_base AS value FROM saltos_base WHERE salto_nombre = ?', [selectedSalto], (error, base) => {
                 console.log("Valoración: " + base[0].value);
+
                                 
                 if (numRows < 7) {
                     rowsCortoLibre.push({
@@ -64,10 +70,8 @@ function addElement(req, res){
     if(codigo == 'CoJ'){
         const selectedSalto = req.body.salto;
         req.getConnection((error, conn) =>{
-            // Variables para form
-            let base_pos =  `base${fila}`;
-            let elemento_pos = `elemento${fila}`;
-            let code = `code${fila}`;
+            // Tenemos en cuenta la incrementación de valor de saltos según el reglamento
+            let base_final;
             // Variables que guardan la puntuación total de los saltos y el array con todos los BASE
             let total_base = 0;
             let base_salto = []
@@ -80,8 +84,45 @@ function addElement(req, res){
                 conn.query('SELECT rating_base AS value FROM saltos_base WHERE salto_nombre = ?', [salto], (error, base) => {
                     // Guardamos el valor en el array y lo sumamos al total
                     base_salto.push(base[0].value);
-                    total_base += base[0].value;
+                    //total_base += base[0].value;
                     console.log("BASE: " + total_base)
+                    base_final = base[0].value;
+
+                    // Comprobamos la categoría y luego los dobles y triples consecutivos
+                    if(i > 0){
+                        let previousSalto = selectedSalto[i-1];
+
+                        //LO COMENTADO ES PARA ALEVIN, INFANTIL Y CADETE DEL PROGRAMA LARGO
+                        /* if(categoria == 'Alevín' || categoria == 'Infantil'){
+                            if (salto.endsWith('_2') && previousSalto.endsWith('_2')){
+                                // Incrementamos el valor un 10%
+                                base_final *= 1.1;  
+                                total_base += base_final;
+                            }
+                        }*/
+
+                        if ((salto.endsWith('_2') && previousSalto.endsWith('_3')) || (salto.endsWith('_3') && previousSalto.endsWith('_2'))){
+                            // Incrementamos el valor un 20%
+                            base_final *= 1.2; 
+                            total_base += base_final; 
+
+                            if(salto == 'axel_2' && previousSalto == 'axel_2'){
+                                // Los doble Axel se consideran triples en este caso
+                                base_final *= 1.3;
+                                total_base += base_final;
+                            }
+                        }
+
+                        if (salto.endsWith('_3') && previousSalto.endsWith('_3')){
+                            // Incrementamos el valor un 30%
+                            base_final *= 1.3;  
+                            total_base += base_final;
+                        }
+                    }
+                    else{
+                        total_base += base[0].value;
+                    }
+                    
 
                     // Comprobamos que estén todos y pasamos la información a la tabla de la coreografía
                     if(base_salto.length == selectedSalto.length){
