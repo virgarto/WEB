c8ecf1e81bd1ce88775ac737cdc172148112b562
virgarto <iamvirginiag@gmail.com>
2024-06-04 17:54:22 +0200

    introducir contraseñas dos veces para editarla

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -26,31 +26,40 @@ function editPatinador (req, res){
         conn.query('SELECT * FROM users WHERE email = ?', [req.session.email] , (err, userData) => {
             
             if(userData.length > 0){
-                // comprobamos contraseña
-                if(data.password && data.password.length > 0){
-                    bcrypt.hash(data.password, 12).then(hash => {
-                        data.password = hash; 
-                        
-                        // Si se ha añadido un nuevo nombre se actualiza username y password
-                        if(data.username && data.username.length > 0){
-                            conn.query('UPDATE users SET username = ?, password = ? WHERE email = ?', [data.username, data.password,req.session.email]);
-                            console.log(data.username);
-                        }
-                        else{
-                            // Actualiza password
-                            conn.query('UPDATE users SET password = ? WHERE email = ?', [data.password, req.session.email]);
-                        }
-                    });
-                }else if(data.username && data.username.length > 0){
+                // Comprobamos que las dos contraseñas se han introducido
+                if(data.password.length > 0 && data.password2.length > 0){
+                    console.log(data.password);
+                    console.log(data.password2);
+
+                    if(data.password === data.password2){
+                        console.log("Contraseñas iguales")
+                        bcrypt.hash(data.password, 12).then(hash => {
+                            data.password = hash; 
+                            
+                            // Si se ha añadido un nuevo nombre se actualiza username y password
+                            if(data.username && data.username.length > 0){
+                                conn.query('UPDATE users SET username = ?, password = ? WHERE email = ?', [data.username, data.password,req.session.email]);
+                                console.log(data.username);
+                            }
+                            else{
+                                // Actualiza password
+                                conn.query('UPDATE users SET password = ? WHERE email = ?', [data.password, req.session.email]);
+                            }
+                        });
+
+                        // Actualizamos las variables de sesion
+                        req.session.name = data.username;
+                        req.session.password = data.password;
+                        res.redirect('/');
+                    }
+                    else{
+                        res.render('editUserForm', {error: 'Error:Las contraseñas no coinciden.'});
+                    }
+                }
+                else if(data.username && data.username.length > 0){
                     // Actualiza username
                         conn.query('UPDATE users SET username = ? WHERE email = ?', [data.username, req.session.email]);
                 }
-
-                // Actualizamos las variables de sesion
-                req.session.name = data.username;
-                req.session.password = data.password;
-                res.redirect('/');
-
             }
             else{
                 res.render('editUserForm', {error: 'Error: No has hecho ningún cambio.'});
